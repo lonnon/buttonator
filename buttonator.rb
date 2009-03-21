8f9f8830767b2e661cbd58f123a504b12a755ce1
Lonnon Foster <lonnon.foster@gmail.com>
2009-03-20 20:59:38 -0700

    Initial commit
    It makes text images! Eeee!
    
    Needs some styling, and I still need the actual specifications for the
    image size and format. Good start, though.

---
+++ buttonator.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'rmagick'
+
+include Magick
+
+class Button
+  def self.create(text)
+    canvas = Image.new(200, 50)
+    type = Draw.new
+    type.fill = 'black'
+    type.gravity = CenterGravity
+    type.font = File.join(File.dirname(__FILE__), 'data/calibrib.ttf')
+    type.pointsize = 30
+    type.annotate(canvas, 0,0,0,0, text)
+    canvas.write(File.join(File.dirname(__FILE__), 'public/button.png'))
+  end
+end
+
+
+get "/" do
+  haml :index
+end
+
+post "/create" do
+  Button.create params[:text] unless params[:text] == ''
+  redirect "/"
+end
