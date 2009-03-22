10393e95e7927aad590b9df8b0c8bdc45a092d15
Lonnon Foster <lonnon.foster@gmail.com>
2009-03-22 00:46:02 -0700

    Added multiple font selection
    Fonts are stored in fonts directory now, not data. Buttonator picks up
    whatever fonts are in the fonts directory and offers them as choices.

--- buttonator.rb
+++ buttonator.rb
@@ -5,25 +5,43 @@ require 'RMagick'
 
 include Magick
 
+ROOT   = File.dirname(__FILE__)
+FONTS  = File.join(ROOT, 'fonts')
+PUBLIC = File.join(ROOT, 'public')
+
 class Button
-  def self.create(text)
+  def self.create(text, font)
     canvas = Image.new(200, 50)
     type = Draw.new
     type.fill = 'black'
     type.gravity = CenterGravity
-    type.font = File.join(File.dirname(__FILE__), 'data/calibrib.ttf')
+    type.font = font
     type.pointsize = 30
     type.annotate(canvas, 0,0,0,0, text)
-    canvas.write(File.join(File.dirname(__FILE__), 'public/button.png'))
+    canvas.write(File.join(PUBLIC, 'button.png'))
+  end
+end
+
+
+helpers do
+  def font_selection
+    fonts = Dir["#{FONTS}/*"].collect do |file|
+      radio = "<input type='radio' name='font' value='#{file}' />"
+      radio += File.basename(file, ".ttf")
+      radio += "<br />"
+    end
+    fonts[0].gsub!("' />", "' checked='checked' />")
+    fonts
   end
 end
 
 
 get "/" do
+  @fonts = font_selection
   haml :index
 end
 
 post "/create" do
-  Button.create params[:text] unless params[:text] == ''
+  Button.create(params[:text], params[:font]) unless params[:text] == ''
   redirect "/"
 end
