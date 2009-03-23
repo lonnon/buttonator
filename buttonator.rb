47bce14fb172fecadb885776c8b2fa2eddae0e1d
Lonnon Foster <lonnon.foster@gmail.com>
2009-03-22 22:43:32 -0700

    Expanded the role of the Font class, added font previews
    Font instances get chucked around now instead of just the filenames of
    the fonts.

--- buttonator.rb
+++ buttonator.rb
@@ -3,45 +3,103 @@ require 'vendor/sinatra/lib/sinatra'
 require 'vendor/haml/lib/haml'
 require 'RMagick'
 
-include Magick
-
 ROOT   = File.dirname(__FILE__)
 FONTS  = File.join(ROOT, 'fonts')
 PUBLIC = File.join(ROOT, 'public')
 
-class Button
-  def self.create(text, font)
-    canvas = Image.new(200, 50)
-    type = Draw.new
-    type.fill = 'black'
-    type.gravity = CenterGravity
-    type.font = font
-    type.pointsize = 30
-    type.annotate(canvas, 0,0,0,0, text)
-    canvas.write(File.join(PUBLIC, 'button.png'))
+module Buttonator
+  class Button
+    def self.make(text, options = {})
+      width     = options[:width] || 200
+      height    = options[:height] || 50
+      fill      = options[:fill] || 'black'
+      gravity   = options[:gravity] || Magick::CenterGravity
+      font      = options[:font] || Buttonator::Font.new(File.join(FONTS, 'calibrib.ttf'))
+      pointsize = options[:pointsize] || 30
+      outfile   = options[:outfile] || File.join(PUBLIC, 'button.png')
+      
+      canvas = Magick::Image.new(width, height)
+      type = Magick::Draw.new
+      type.fill = fill
+      type.gravity = gravity
+      type.font = font.filename
+      type.pointsize = pointsize
+      type.annotate(canvas, 0,0,0,0, text)
+      canvas.write(outfile)
+    end
+  end
+
+  class Font
+    def initialize(file)
+      @filename = file
+    end
+    
+    def filename
+      @filename
+    end
+    
+    def name
+      File.basename(@filename, ".ttf")
+    end
+    
+    def image_name
+      "font_#{name}.png"
+    end
+    
+    def sample_image
+      Buttonator::Button.make("hamburgefontsiv",
+                              :outfile => File.join(PUBLIC, image_name),
+                              :width => 150, :height => 20,
+                              :font => self, :pointsize => 14,
+                              :gravity => Magick::SouthWestGravity)
+    end
+    
+    def radio_button(selected = false)
+      if selected
+        checked = "checked='checked' "
+      else
+        checked = ''
+      end
+      image = File.join(PUBLIC, image_name)
+      sample_image
+      radio = "<input type='radio' name='font' value='#{@filename}' #{checked}/>"
+      radio += name + "&nbsp;"
+      radio += "<img src='#{image_name}' width='150' height='20' />"
+      radio += "<br />"
+    end
   end
 end
 
-
 helpers do
-  def font_selection
-    fonts = Dir["#{FONTS}/*"].collect do |file|
-      radio = "<input type='radio' name='font' value='#{file}' />"
-      radio += File.basename(file, ".ttf")
-      radio += "<br />"
+  def get_fonts
+    fonts = []
+    Dir["#{FONTS}/*"].each do |file|
+      fonts.push(Buttonator::Font.new(file))
     end
-    fonts[0].gsub!("' />", "' checked='checked' />")
     fonts
+  end
+  
+  def font_selection(fonts)
+    first_font = fonts.shift
+    font_inputs = first_font.radio_button(true)
+    fonts.each do |font|
+      font_inputs += font.radio_button
+    end
+    font_inputs
   end
 end
 
 
 get "/" do
-  @fonts = font_selection
+  @fonts = get_fonts
+  @font_selection = font_selection(@fonts)
   haml :index
 end
 
 post "/create" do
-  Button.create(params[:text], params[:font]) unless params[:text] == ''
+  unless params[:text] == ''
+    Buttonator::Button.make(params[:text],
+                                   :font => Buttonator::Font.new(params[:font]))
+  end
   redirect "/"
 end
